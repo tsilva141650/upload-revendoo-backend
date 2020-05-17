317c0941ec39a2e1ccd0fa1a4182896ee9708666
Tiago Silva <tiago.ap.silva84@gmail.com>
2020-05-17 17:48:58 -0300

    Adiciona Procfile

--- src/controllers/CatalogoController.js
+++ src/controllers/CatalogoController.js
@@ -1,24 +1,20 @@
 const axios = require('axios');
 const Catalogo = require('../models/Catalogo');
-const Usuario = require('../models/Usuario');
+//const Usuario = require('../models/Usuario');
 
 module.exports ={
  
     async index(req,res){
         
         const {catalogo} = req.headers;
-        
-        const catalogos = await Catalogo.find({
-            $and:[
-                { _id:{$eq:catalogo}}, 
-            ],
-        });
 
-        if(!catalogos){
+        const _catalogo = await Catalogo.findById(catalogo);
+
+        if(!_catalogo){
             return res.status(400).json({error:'Não existe catalogo para este usuário.'});
         }
 
-        return res.json(catalogos);
+        return res.json(_catalogo);
     },
     
 
@@ -26,16 +22,16 @@ module.exports ={
     async store(req,res){
        
         const {name, produtos} = req.body;
-        const {usuario} = req.headers;
+        //const {usuario} = req.headers;
 
         
-        const loggedUsuario = await Usuario.findById(usuario);
+        //const loggedUsuario = await Usuario.findById(usuario);
 
         const catalogo = await Catalogo.create({
             name, produtos
         });
 
-        loggedUsuario.catalogos.push(catalogo._id);
+        //loggedUsuario.catalogos.push(catalogo._id);
 
         return res.json(catalogo);
     }
