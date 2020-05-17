88dbd1e0b9d89f52c1c18bfeef46a8d213410768
Tiago Silva <tiago.ap.silva84@gmail.com>
2020-05-17 12:05:00 -0300

    Adiciona Procfile

--- src/server.js
+++ src/server.js
@@ -10,6 +10,6 @@ server.use(cors());
 server.use(express.json())
 server.use(routes);
 
-server.listen(3333);
+//server.listen(3333);
 
-//server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000);
